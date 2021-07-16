d37a4ece9d3ac9b0af2fb1c08c9096dde0441bff
Jimmy-Yu-git <77708964+Jimmy-Yu-git@users.noreply.github.com>
2021-07-16 18:30:55 +0800

    new

--- ChatAPP/frontend/src/graphql/index.js
+++
@@ -1,5 +0,0 @@
-export { CREATE_CHATBOX_MUTATION } from './mutation';
-export {CREATE_MESSAGE_MUTATION} from './Mutationmess'
-export {CHATBOX_QUERY} from './query'
-export {MESSAGE_SUBSCRIPTION} from './messagesubscribe'
-export {AllMessage_Sub} from './allmesssub'
